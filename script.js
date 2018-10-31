a7ede9dc5fd91b7b5903f72fdb174522a120f2af
hanna_nyman <hannanyman.sk@gmail.com>
2018-10-31 14:28:07 +0100

    Added variables for HTML elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// DOM variables
+let firstDiv = document.getElementById('first');
+let secondDiv = document.getElementById('second');
+let thirdDiv = document.getElementById('third');
+let fourthDiv = document.getElementById('fourth');
