ba42a6e3cf431bc1b5b45492c279b5169e84b9e3
hanna_nyman <hannanyman.sk@gmail.com>
2018-10-31 15:18:37 +0100

    Adding function to start the test, also added global variables

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // DOM variables
-let firstDiv = document.getElementById('first');
-let secondDiv = document.getElementById('second');
-let thirdDiv = document.getElementById('third');
-let fourthDiv = document.getElementById('fourth');
+let firstDiv = document.querySelector('#first');
+let secondDiv = document.querySelector('#second');
+let thirdDiv = document.querySelector('#third');
+let fourthDiv = document.querySelector('#fourth');
+let input = document.querySelector('#input');
+let bar = document.querySelector('#bar');
+let barInside = document.querySelector('#bar_inside');
+let p_generatedNumbers = document.querySelector('.p_generated_numbers');
+let p_level = document.querySelector('#p_level');
+let p_userguess = document.querySelector('#p_userguess');
+
+// global variables
+let numbersAmount = 1;
+let generatedNumbers = [];
+
+const startTest = () => {
+    for (let i = 0; i < generatedNumbers; i++) {
+        let number = Math.floor(Math.random() * 10);
+        generatedNumbers.push(number);
+    }
+}
